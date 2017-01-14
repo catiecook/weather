04293fb4045da8497a2c006b8ab66d008e9c5ce1
catiecook <catiecook888@gmail.com>
2017-01-14 11:50:55 -0500

    pushing to heroku

--- public/javascripts/location.js
+++ public/javascripts/location.js
@@ -1,36 +1,13 @@
-//get location
-// locationPromise()
-//
-// var coords = getLocation()
-// console.log(coords);
-// console.log("hello");
-
-// Promise.all(coords).then((data)=>{
-//   console.log(data);
-// })
 
 getLocation()
 
-
 function getLocation() {
-  // return new Promise((resolve, reject) => {
-    $.getJSON('https://ipinfo.io/geo', function(response) {
-      var loc = response.loc.split(',');
-      var coords = {
-        latitude: loc[0],
-        longitude: loc[1]
-      }
-      console.log(coords);
-      getWeather(coords.latitude, coords.longitude)
-      })
-    // })
+  $.getJSON('https://ipinfo.io/geo', function(response) {
+    var loc = response.loc.split(',');
+    var coords = {
+      latitude: loc[0],
+      longitude: loc[1]
+    }
+    getWeather(coords.latitude, coords.longitude)
+    })
 }
-
-
-
-//   .then((data) => {
-//     getWeather(data.lat, data.lon);
-//   })
-//   .catch((err) => {
-//     console.log(err);
-//   })
