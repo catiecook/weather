d2f65060ede3bd6ae38e3364ed65ac2aca3ceea0
catiecook <catiecook888@gmail.com>
2017-01-14 18:34:58 -0500

    preparing for file compressing and the big merge

--- app.js
+++ app.js
@@ -6,20 +6,6 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var dotenv = require('dotenv').config();
 
-//*******************************
-//http-proxy data
-var httpProxy = require('http-proxy');
-var http = require('http');
-// Create your proxy server and set the target in the options.
-httpProxy.createProxyServer({target:'http://localhost:9000'}).listen(8000);
-// Create your target server
-http.createServer(function (req, res) {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.write('request successfully proxied!' + '\n' + JSON.stringify(req.headers, true, 2));
-  res.end();
-}).listen(9000);
-//*******************************
-
 var index = require('./routes/index');
 var users = require('./routes/users');
 
